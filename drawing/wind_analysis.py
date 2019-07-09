0f9c6caf3172dd7df112058442fb20eeb02f9547
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-08 17:14:18 -0700

    updating import for settings for newer django

--- drawing/wind_analysis.py
+++ drawing/wind_analysis.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 #from madrona.raster_stats.models import RasterDataset, zonal_stats
+from django.conf import settings
 from settings import *
 from general.utils import default_value, sq_meters_to_sq_miles
 from .models import *
@@ -9,12 +10,12 @@ from .models import *
 '''
 def display_wind_analysis(request, wes, template='wind/reports/wind_report.html'):
     context = get_wind_analysis(wes)
-    return render_to_response(template, RequestContext(request, context)) 
+    return render_to_response(template, RequestContext(request, context))
 
 '''
 Run the analysis, create the cache, and return the results as a context dictionary so they may be rendered with template
-'''    
-def get_wind_analysis(wes): 
+'''
+def get_wind_analysis(wes):
     #compile context
     area = sq_meters_to_sq_miles(wes.geometry_final.area)
     context = { 'wes': wes, 'default_value': default_value, 'area': area }
