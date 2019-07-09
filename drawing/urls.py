f58cd113e9b024ce62ee3a147581e38f767e0141
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-09 15:31:56 -0700

    django 2 url patterns update

--- drawing/urls.py
+++ drawing/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url, patterns
+# from django.conf.urls import url, patterns
+from django.urls import re_path, include
 from .views import *
 
-urlpatterns = patterns('',
+urlpatterns = [
+    #'',
     #drawings
-    url(r'^get_drawings$', get_drawings),
+    re_path(r'^get_drawings$', get_drawings),
     #feature reports
-    url(r'^wind_report/(\d+)', wind_analysis, name='wind_analysis'), #user requested wind energy site analysis 
-    url(r'^aoi_report/(\d+)', aoi_analysis, name='aoi_analysis'), #user requested area of interest analysis 
-)
+    re_path(r'^wind_report/(\d+)', wind_analysis, name='wind_analysis'), #user requested wind energy site analysis
+    re_path(r'^aoi_report/(\d+)', aoi_analysis, name='aoi_analysis'), #user requested area of interest analysis
+]
